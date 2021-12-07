6d24a6c3061014c0800e2e85281009ae85d474a1
mariechantallie <90793133+mariechantallie@users.noreply.github.com>
2021-12-06 23:36:01 -0800

    Sphere colliders were deleted, prefabs were created, and new movement code was begun
    I went through all of our objects and removed sphere colliders, replacing them with Mesh colliders. I also deleted the old player prefab and created a new one while starting on a much more in depth movement script. Stopping here for the night, will have player movement by tomorrow morning.

---
+++ Undying Loyalty/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    [field:SerializeField]
+
+    public bool IsGrounded {get; private set; } = true;
+    // Start is called before the first frame update
+    
+    
+        
+    
+}
--- Undying Loyalty/Assets/Scripts/Movement.cs
+++ Undying Loyalty/Assets/Scripts/Movement.cs
@@ -2,25 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
-{
-    [SerializeField] float MoveSpeed = 1f;
-    void Start()
-    {
-        
+
+[RequireComponent(typeof(Rigidbody), typeof(Collider))]
+public class Movement : MonoBehaviour {
+    [SerializeField] float groundedSpeed;
+    [SerializeField] float maxSpeed;
+    [SerializeField] float inAirSpeed;
+    [SerializeField] float fallSpeed;
+    [SerializeField] float runSpeed;
+    [SerializeField] KeyCode runKey = KeyCode.LeftShift;
+    [SerializeField] float jumpForce;
+    [SerializeField] KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] float breakForce;
+    [SerializeField] GroundCheck groundCheck;
+
+    Rigidbody playerRigidBody;
+
+    Vector3 movement = Vector3.zero;
+
+    void Awake() {
+        playerRigidBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        MovePlayer();
-    }
-     void MovePlayer()
-    {
-        float xValue = Input.GetAxis("Horizontal")*Time.deltaTime*MoveSpeed;
-        float zValue = Input.GetAxis("Vertical")*Time.deltaTime*MoveSpeed;
-        transform.Translate(xValue,0,zValue);
+    void Update() {
+        var input = Input.GetAxis("Horizontal");
 
+        if(input == 0f) {
+            HitTheBrakes();
+        } else {
+            Move(input);
+        }
+
+
+        if(Input.GetKeyDown(jumpKey)) {
+            HandleJump();
+        }
     }
-   
+
+    private void FixedUpdate() {
+        if(playerRigidBody.velocity.y < 0f) {
+            playerRigidBody.AddForce(Vector3.down * fallSpeed * Time.deltaTime, ForceMode.Acceleration);
+        }
+    }
+
+    void HandleJump() {
+        if(groundCheck.IsGrounded) { Jump(); }
+    }
+
+    void Jump() {
+        playerRigidBody.AddForce(Vector3.up * jumpForce);
+    }
+
+    void HitTheBrakes() {
+        if(!groundCheck.IsGrounded && playerRigidBody.velocity.magnitude > 0f) {
+            playerRigidBody.AddForce(-playerRigidBody.velocity * breakForce * Time.deltaTime);
+        }
+    }
+
+    void Move(float input) {
+        if(playerRigidBody.velocity.magnitude < maxSpeed) {
+            playerRigidBody.AddForce(Vector3.forward * input * MoveSpeed() * Time.deltaTime);
+        }
+    }
+
+    public float MoveSpeed() {
+        float result = 0f;
+        if(groundCheck.IsGrounded) {
+            result = Input.GetKey(runKey) ? runSpeed : groundedSpeed;
+        } else {
+            result = inAirSpeed;
+        }
+
+        return result;
+    }
 }
